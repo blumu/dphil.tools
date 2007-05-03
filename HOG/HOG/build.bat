ffdbc60770e99dd8894fe8e444eb5ab651ae63c1
William Blum <william.blum@gmail.com>
2007-05-03 22:00:42 +0000

    git-svn-id: svn://localhost@56 b57001f7-1fde-174e-91bb-cd1118d97a9f

--- HOG/HOG/build.bat
+++ HOG/HOG/build.bat
@@ -1,4 +1,6 @@
 @if "%_echo%"=="" echo off
+
+:setenvir
 setlocal
 if "%FSHARP_HOME%"=="" ( set FSHARP_HOME=C:\Program Files\FSharp-1.9.1.8)
 if "%FSC%"=="" ( set FSC=%FSHARP_HOME%\bin\fsc.exe )
@@ -6,15 +8,16 @@ if "%FSYACC%"=="" ( set FSYACC=%FSHARP_HOME%\bin\fsyacc.exe )
 if "%FSLEX%"=="" ( set FSLEX=%FSHARP_HOME%\bin\fslex.exe )
 if "%GLEE%"=="" ( set GLEE=C:\Program Files\Microsoft Research\GLEE)
 
+:compile
 """%FSC%""" --fullpaths --progress -Ooff -I "%GLEE%\bin" -r Microsoft.GLEE.dll -r Microsoft.GLEE.GraphViewerGDI.dll -r Microsoft.GLEE.Drawing.dll -o hog.exe --no-warn 40 --target-winexe -g hog.mli hog.ml hocpda.ml myform.fs cpdaform.fs myform.resx
 
-if ERRORLEVEL 1 goto Exit
+if ERRORLEVEL 1 goto exit
 
 echo ********************************************
 echo Built ok, you may now run Hog.exe
 echo ********************************************
 
-:Exit
+:exit
 endlocal
 rem pause
 exit /b %ERRORLEVEL%
