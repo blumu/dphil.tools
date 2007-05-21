80bbcd9de4db3a31cb237483a9cdb3f3a267939a
William Blum <william.blum@gmail.com>
2007-05-21 17:49:51 +0000

    git-svn-id: svn://localhost@71 b57001f7-1fde-174e-91bb-cd1118d97a9f

--- HOG/zip.cmd
+++
@@ -1,3 +0,0 @@
-del hog.zip 
-7z -tzip -r a hog.zip *.fs *.fsi *.ml *.mli *.mll *.mly *.bat *.cmd *.txt *.rs *.fsharp *.csproj *.cs *.settings *.resx *.sln 
-pause
---
+++ HOG/zip_src.cmd
@@ -0,0 +1,3 @@
+del hog_src.zip 
+7z -tzip -r a hog_src.zip *.fs *.fsi *.ml *.mli *.mll *.mly *.bat *.cmd *.txt *.rs *.fsharp *.csproj *.cs *.settings *.resx *.sln 
+pause
