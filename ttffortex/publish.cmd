ee393c55ac3ea0f8d92168cc0bd68a19ba3b365a
William Blum <william.blum@gmail.com>
2007-11-09 07:31:30 +0000

    git-svn-id: svn://localhost@113 b57001f7-1fde-174e-91bb-cd1118d97a9f

--- ttffortex/publish.cmd
+++ ttffortex/publish.cmd
@@ -1,3 +1,4 @@
 copy ttffortex.zip d:\documents\website\software\latexttf
+copy ttffortex.cmd d:\documents\website\software\latexttf
 pscp d:\documents\website\software\latexttf\* william@www.famille-blum.org:public_html/software/latexttf
 pause
