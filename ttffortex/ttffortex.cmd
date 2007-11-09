c6b7a541e6f3455575c3f36a78b6fc8fccc14894
William Blum <william.blum@gmail.com>
2007-11-09 01:19:20 +0000

    git-svn-id: svn://localhost@108 b57001f7-1fde-174e-91bb-cd1118d97a9f

---
+++ ttffortex/ttffortex.cmd
@@ -0,0 +1,157 @@
+@rem This line serves to eat the BOM (byte-order-mark, present if the cmd file is saved by Notepad in UTF-8 mode)
+@echo OFF
+rem created by William Blum on 22 fev 2006
+rem  Install a truetype unicode font for latex and pdflatex
+rem requirement:
+rem   - ttf2tfm, ttfucs.sty and utf8ttf.def
+
+echo Truetype font installer for Latex by William Blum.
+echo Version 1.1 - 8/11/2007
+echo.
+
+setlocal
+
+if (%1)==() goto print_syntax
+set TFFFILE_PATH=%1
+set TFFFILE_BASENAME=%~n1
+rem Short name for the font (6 characters maximum) used to generate the files "baseXX.enc"
+set TFFFILE_SHORT_BASENAME=%TFFFILE_BASENAME:~0,6%
+
+set TEXMF=%2
+if (%TEXMF%)==() goto detect_miktex_dir:
+
+echo The font will be installed in the directory: %TEXMF%
+goto get_pideid
+
+:detect_miktex_dir
+rem Guess miktex dir: look for a directory in the PATH env var containing pdflatex.exe.
+FOR %%I IN (pdflatex.exe) do set TEXMF="%%~$PATH:I"
+
+rem default value
+if (%TEXMF%)==("") set TEXMF="C:\Program files\Miktex 2.7\" & goto miktex_dir_ok
+set TEXMF=%TEXMF:miktex\bin\pdflatex.exe=%
+echo MiKTex directory detected: %TEXMF%
+goto get_pideid
+
+:get_pideid
+set PID=%3
+rem default value
+if (%PID%)==() set PID=3
+
+set EID=%4
+rem default value
+if (%EID%)==() set EID=1
+
+
+rem Check that the font file extension is .ttf (/i for case insensitive comparison)
+IF /i "%~x1" NEQ ".ttf" goto badfileextension
+IF NOT EXIST %TFFFILE_PATH% goto filenotfound
+IF NOT EXIST %TEXMF% goto dirnotfound
+
+
+echo Install the truetype font "%TFFFILE_BASENAME%.TTF" in the latex directory %TEXMF% ...
+
+@echo ON
+@if not exist %TEXMF%\fonts\truetype\ mkdir %TEXMF%\fonts\truetype\
+copy %TFFFILE_PATH% %TEXMF%\fonts\truetype\
+cd %TEXMF%\fonts\truetype
+ttf2tfm %TFFFILE_BASENAME%.ttf -P %PID% -E %EID% -w %TFFFILE_SHORT_BASENAME%@Unicode@ > ttfortex.log ||  goto ttf2tfm_error
+@if exist %TFFFILE_BASENAME%.map del %TFFFILE_BASENAME%.map
+for %%i in (*.enc) do @echo %%~ni ^<%TFFFILE_BASENAME%.ttf ^<%%~ni.enc >>%TFFFILE_BASENAME%.map
+@if not exist %TFFFILE_BASENAME%.map goto nomap
+
+@if not exist %TEXMF%\pdftex\config\ mkdir %TEXMF%\pdftex\config\
+move %TFFFILE_BASENAME%.map %TEXMF%\pdftex\config\
+
+@if not exist %TEXMF%\fonts\tfm\%TFFFILE_BASENAME% mkdir %TEXMF%\fonts\tfm\%TFFFILE_BASENAME%
+move %TFFFILE_SHORT_BASENAME%*.tfm %TEXMF%\fonts\tfm\%TFFFILE_BASENAME%\
+
+@if not exist %TEXMF%\pdftex\enc\%TFFFILE_BASENAME% mkdir %TEXMF%\pdftex\enc\%TFFFILE_BASENAME%
+move %TFFFILE_SHORT_BASENAME%*.enc %TEXMF%\pdftex\enc\%TFFFILE_BASENAME%\
+
+@if not exist %TEXMF%\tex\latex\winfonts mkdir %TEXMF%\tex\latex\winfonts
+@set FDFILE=%TEXMF%\tex\latex\winfonts\C70%TFFFILE_BASENAME%.fd
+@echo \ProvidesFile{C70%TFFFILE_BASENAME%.fd}[%TFFFILE_BASENAME%] > %FDFILE%
+@echo \DeclareFontFamily{C70}{%TFFFILE_BASENAME%}{\hyphenchar \font\m@ne} >> %FDFILE%
+@echo \DeclareFontShape{C70}{%TFFFILE_BASENAME%}{m}{n}{^<-^> CJK * %TFFFILE_BASENAME%}{} >> %FDFILE%
+@echo \DeclareFontShape{C70}{%TFFFILE_BASENAME%}{bx}{n}{^<-^> CJKb * %TFFFILE_BASENAME%}{\CJKbold}	>> %FDFILE%
+@echo \endinput >> %FDFILE%
+
+initexmf --update-fndb
+
+@goto succeed
+
+:print_syntax
+@echo off
+echo The syntax is:
+echo. %0 ttffile [texmfdir] [pid] [eid]
+echo  . ttffile is the full path to the true-type font (e.g. c:\WINDOWS\Fonts\simhei.ttf)
+echo  . texmfdir is the local root latex directory (where you want to install the font; if no argument is provided then it uses the Miktex directory defined in the PATH environment variable; do NOT use a relative path)
+echo  . pid is the platform id of the font (used by ttf2tfm)
+echo  . pid is the encoding id of the font (used by ttf2tfm)
+echo.
+goto end
+
+:filenotfound
+@echo off
+echo File "%TFFFILE_PATH%" not found!
+SET v_return=1
+goto end
+
+:badfileextension
+@echo off
+echo Bad file extension ("%TFFFILE_PATH%")! The font file must have the .TTF extension.
+SET v_return=4
+goto end
+
+:dirnotfound
+@echo off
+echo The directory "%TEXMF%" does not exist!
+SET v_return=2
+goto end
+
+:ttf2tfm_error
+@echo off
+echo Problem during the execution of ttf2tfm. Try to specify parameter PID and EID.
+SET v_return=3
+goto end
+
+:nomap
+@echo off
+echo Problem during the generation of the map file.
+SET v_return=4
+goto end
+
+:succeed
+@echo off
+echo.
+echo Operation succeeded. The truetype font has been installed to your LaTeX environment!
+echo You can use it in an TeX document as follows:
+echo   \usepackage[utf8ttf]{inputenc}
+echo   \usepackage{ttfucs}
+echo   \DeclareTruetypeFont{%TFFFILE_BASENAME%}{%TFFFILE_SHORT_BASENAME%}
+echo   \begin{document}
+echo   \TruetypeFont{%TFFFILE_BASENAME%}
+echo   unicode texte
+echo   \end{document}
+SET v_return=0
+
+
+:end
+
+endlocal
+
+rem Utilisation sous pdflatex:
+rem necessite les fichiers ttfucs.sty et utf8ttf.def
+
+rem \usepackage[utf8ttf]{inputenc}
+rem \usepackage{ttfucs}
+rem \DeclareTruetypeFont{cyberb}{cyberb}
+rem \DeclareTruetypeFont{simkay}{simkay}
+
+rem \begin{document}
+rem \TruetypeFont{cyberbit}
+rem 中文 大
+rem \TruetypeFont{simkai}
+rem 日本小
+rem \end{document}
---
+++ ttffortex/ttffortex10.cmd
@@ -0,0 +1,127 @@
+﻿@echo OFF
+rem created by William Blum on 22 fev 2006
+rem modified on 26 may 2006
+rem  Install a truetype unicode font for latex and pdflatex
+rem requirement:
+rem   - ttf2tfm, ttfucs.sty and utf8ttf.def
+
+echo Truetype font installer for Latex by William Blum.
+echo Version 1.0 - 22/02/2006
+echo.
+
+setlocal
+set FONT=%1
+set PID=3
+set EID=1
+set FONTTTF=%1.ttf
+set TEXMF=\localtexmf
+
+if [%1] EQU [] goto print_syntax
+if [%2] NEQ [] set TEXMF=%2
+if [%3] NEQ [] set PID=%3 
+if [%4] NEQ [] set EID=%4
+
+IF NOT EXIST %FONTTTF% goto filenotfound
+IF NOT EXIST %TEXMF% goto dirnotfound
+
+
+echo Install the truetype font "%FONTTTF%" in the latex directory "%TEXMF%" ...
+
+echo ON
+mkdir %TEXMF%\fonts\truetype\
+copy %FONTTTF% %TEXMF%\fonts\truetype\
+cd %TEXMF%\fonts\truetype
+ttf2tfm %FONTTTF% -P %PID% -E %EID% -w %FONT%@Unicode@ ||  goto ttf2tfm_error
+del %FONT%.map
+for %%i in (*.enc) do @echo %%~ni ^<%FONTTTF% ^<%%~ni.enc >>%FONT%.map
+@if not exist %FONT%.map goto nomap
+
+@if not exist %TEXMF%\pdftex\config\ mkdir %TEXMF%\pdftex\config\
+move %FONT%.map %TEXMF%\pdftex\config\
+
+@if not exist %TEXMF%\fonts\tfm\%FONT% mkdir %TEXMF%\fonts\tfm\%FONT%
+move %FONT%*.tfm %TEXMF%\fonts\tfm\%FONT%\
+
+@if not exist %TEXMF%\pdftex\enc\%FONT% mkdir %TEXMF%\pdftex\enc\%FONT%
+move %FONT%*.enc %TEXMF%\pdftex\enc\%FONT%\
+
+@if not exist %TEXMF%\tex\latex\winfonts mkdir %TEXMF%\tex\latex\winfonts
+@set FDFILE=%TEXMF%\tex\latex\winfonts\C70%FONT%.fd
+@echo \ProvidesFile{C70%FONT%.fd}[%FONT%] > %FDFILE%
+@echo \DeclareFontFamily{C70}{%FONT%}{\hyphenchar \font\m@ne} >> %FDFILE%
+@echo \DeclareFontShape{C70}{%FONT%}{m}{n}{^<-^> CJK * %FONT%}{} >> %FDFILE%
+@echo \DeclareFontShape{C70}{%FONT%}{bx}{n}{^<-^> CJKb * %FONT%}{\CJKbold}	>> %FDFILE%
+@echo \endinput >> %FDFILE%
+
+initexmf --update-fndb
+
+goto succeed
+
+:print_syntax
+@echo off
+echo The syntax is:
+echo. %0 ttffile [texmfdir] [pid] [eid]
+echo  . ttffile is the base name of the true type font (without .ttf)
+echo  . texmfdir is the latex directory where you want to install the font (localtexmf for instance)
+echo  . pid is the platform id of the font (used by ttf2tfm)
+echo  . pid is the encoding id of the font (used by ttf2tfm)
+echo.
+goto end
+
+:filenotfound
+@echo off
+echo File "%FONTTTF%" not found!
+SET v_return=1
+goto end
+
+:dirnotfound
+@echo off
+echo The directory "%TEXMF%" does not exist!
+SET v_return=2
+goto end
+
+:ttf2tfm_error
+@echo off
+echo Problem during the execution of ttf2tfm. Try to specify parameter PID and EID.
+SET v_return=3
+goto end
+
+:nomap
+@echo off
+echo Problem during the generation of the map file.
+SET v_return=4
+goto end
+
+:succeed
+echo off
+echo.
+echo Operation succeeded. The truetype font has been installed!
+echo You can use in as follow in a tex file:
+echo   \usepackage[utf8ttf]{inputenc}
+echo   \usepackage{ttfucs}
+echo   \DeclareTruetypeFont{%FONT%}{%FONT%}
+echo   \begin{document}
+echo   \TruetypeFont{%FONT%}
+echo   unicode texte
+echo   \end{document}
+SET v_return=0
+
+
+:end
+
+endlocal
+
+rem Utilisation sous pdflatex:
+rem necessite les fichiers ttfucs.sty et utf8ttf.def
+
+rem \usepackage[utf8ttf]{inputenc}
+rem \usepackage{ttfucs}
+rem \DeclareTruetypeFont{cyberb}{cyberb}
+rem \DeclareTruetypeFont{simkay}{simkay}
+
+rem \begin{document}
+rem \TruetypeFont{cyberbit}
+rem 中文 大
+rem \TruetypeFont{simkai}
+rem 日本小
+rem \end{document}
