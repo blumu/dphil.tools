71b4c871d49d57ede01c436f25b8af23b556f81b
William Blum <william.blum@gmail.com>
2007-11-11 17:04:36 +0000

    git-svn-id: svn://localhost@115 b57001f7-1fde-174e-91bb-cd1118d97a9f

--- ttffortex/zip.cmd
+++ ttffortex/zip.cmd
@@ -1,7 +1,8 @@
 pushd utf_example
 latex latex_utf8.tex
+pdflatex latex_utf8.tex
 pdflatex pdflatex_utf8.tex
 popd
 del ttffortex.zip 
-7za -tzip a ttffortex.zip ttffortex.cmd utf_example\utf8ttf.def utf_example\ttfucs.sty utf_example\latex_utf8.tex utf_example\pdflatex_utf8.tex utf_example\pdflatex_utf8.pdf utf_example\latex_utf8.dvi
+7za -tzip a ttffortex.zip ttffortex.cmd utf_example\utf8ttf.def utf_example\ttfucs.sty utf_example\latex_utf8.tex utf_example\pdflatex_utf8.tex utf_example\pdflatex_utf8.pdf utf_example\latex_utf8.dvi utf_example\latex_utf8.pdf
 @IF ERRORLEVEL 1 pause
