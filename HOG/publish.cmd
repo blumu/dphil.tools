2562710a18e850008631977a9bbc40e5f3a3c724
William Blum <william.blum@gmail.com>
2007-12-04 23:44:27 +0000

    git-svn-id: svn://localhost@130 b57001f7-1fde-174e-91bb-cd1118d97a9f

--- HOG/publish.cmd
+++ HOG/publish.cmd
@@ -1,13 +1,13 @@
-pushd HOG
 ::omake MONO=
 ::omake
-popd
+
 
 pscp hog_exe.zip hog_src.zip blum@mercury.comlab.ox.ac.uk:/auto/fs/websrc/oucl/work/william.blum/
 @if ERRORLEVEL 1 goto exit
 
 plink blum@mercury.comlab.ox.ac.uk /users/blum/packbin/webpub /auto/fs/websrc/oucl/work/william.blum/hog_exe.zip /auto/fs/websrc/oucl/work/william.blum/hog_src.zip
 @if ERRORLEVEL 1 goto exit
+
 
 :success
 echo **************** Program built and published.
---
+++ HOG/publish_fcsharpomake.cmd
@@ -0,0 +1,14 @@
+7z -tzip -r a fcsharp_omake.zip *.om
+copy fcsharp_omake.zip d:\documents\website\research
+pscp fcsharp_omake.zip william@www.famille-blum.org:public_html/research/
+@if ERRORLEVEL 1 goto exit
+
+:success
+echo **************** fcsharp_omake has been published successfuly.
+
+:exit
+endlocal
+@if ERRORLEVEL 1 pause
+exit /b %ERRORLEVEL%
+
+
