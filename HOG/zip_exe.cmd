118636c76bc25b48d6345f8a09869fbdb1c83cca
William Blum <william.blum@gmail.com>
2008-01-24 17:09:45 +0000

    git-svn-id: svn://localhost@161 b57001f7-1fde-174e-91bb-cd1118d97a9f

--- HOG/zip_exe.cmd
+++ HOG/zip_exe.cmd
@@ -1,3 +1,3 @@
 del hog_exe.zip 
-7z -tzip -r a hog_exe.zip -xr!.svn bin\Release\hog.exe bin\Release\Microsoft.GLEE.* examples\
+7z -tzip -r a hog_exe.zip -xr!.svn bin\Release\*.exe bin\Release\*.dll bin\Release\Microsoft.GLEE.* examples\
 @if ERRORLEVEL 1 pause
--- HOG/zip_src.cmd
+++ HOG/zip_src.cmd
@@ -1,3 +1,3 @@
 del hog_src.zip 
-7z -tzip -r a hog_src.zip *.fs *.fsi *.ml *.mli *.mll *.mly *.bat Hog\*.cmd *.txt *.rs *.fsharp *.csproj *.cs *.settings *.resx *.sln *.om *.lmd OMakefile OMakeroot README
+7z -tzip -r a -xr!.svn hog_src.zip *.fs *.fsi *.ml *.mli *.mll *.mly *.bat Hog\*.cmd *.txt *.rs *.fsharp *.csproj *.cs *.settings *.resx *.sln *.om *.lmd OMakefile OMakeroot README GUI\Resources\*
 @if ERRORLEVEL 1 pause
