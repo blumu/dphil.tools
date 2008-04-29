73558285d184c7ff83ddf27ae1d9cb8c46c5f487
William Blum <william.blum@gmail.com>
2008-04-29 23:13:55 +0000

    LatexDameon branch moved to Google Code: http://code.google.com/p/latexdaemon/
    git-svn-id: svn://localhost@210 b57001f7-1fde-174e-91bb-cd1118d97a9f

--- latexdaemon/build.cmd
+++
@@ -1,5 +0,0 @@
-:: make sure the current directory is set to the directory containing this batch file
-cd /d %0\..
-::%VSDEVENV% /Rebuild Release latexdaemon-vc80.vcproj
-%VSDEVENV% /Rebuild Release latexdaemon.vcproj
-pause
--- latexdaemon/publish.cmd
+++
@@ -1,4 +0,0 @@
-copy latexdaemon.zip %USERPROFILE%\documents\website\software\latexdaemon
-copy changelog-inc.html %USERPROFILE%\documents\website\software\latexdaemon
-pscp %USERPROFILE%\documents\website\software\latexdaemon\* william@www.famille-blum.org:public_html/software/latexdaemon
-pause
