68820642f0717b7dbfa18ce0eed59fda8311d1a8
William Blum <william.blum@gmail.com>
2008-11-04 17:43:12 +0000

    git-svn-id: svn://localhost@219 b57001f7-1fde-174e-91bb-cd1118d97a9f

--- HOG/zip_src.cmd
+++ HOG/zip_src.cmd
@@ -1,3 +1,3 @@
 del hog_src.zip 
-7za -tzip -r a -xr!.svn hog_src.zip *.fs *.fsi *.ml *.mli *.mll *.mly *.bat Hog\*.cmd *.txt *.rs *.fsharp *.csproj *.cs *.settings *.resx *.sln *.om *.lmd OMakefile OMakeroot README Changelog.txt GUI\Resources\*
+7za -tzip -r a -xr!.svn hog_src.zip *.fs *.fsi *.ml *.mli *.mll *.mly *.bat Hog\*.cmd *.txt *.rs *.fsproj *.csproj *.cs *.settings *.resx *.sln *.om *.lmd OMakefile OMakeroot README Changelog.txt GUI\Resources\*
 @if ERRORLEVEL 1 pause
